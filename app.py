891086a61b7b825935463aabb42071fed8874c09
Arcanine02 <santiagoanaya@ciencias.unam.mx>
2024-12-06 16:04:20 -0600

    Update app.py

--- app.py
+++ app.py
@@ -283,7 +283,7 @@ min_vol_port_stats = list(zip(stats, around(portfolio_stats(min_vol_port['x']),4
 @st.cache_data
 def efficient_frontier():
  # Efficient frontier params
- targetrets = linspace(0.1,0.60,100)
+ targetrets = linspace(0.1,0.30,20)
  tvols = []
  weights_list = []
  
